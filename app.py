7b835ab0355dc2a1c9d4b6ec21dc27277799eb92
erikkov <erikkov@ulrik.uio.no>
2025-03-23 18:32:34 +0100

    Heroku config

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from __init__ import app
+import os
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #Heroku config
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+    #Local config
+    #app.run(debug=True)
